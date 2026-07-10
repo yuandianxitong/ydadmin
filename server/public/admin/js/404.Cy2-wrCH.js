a8d011be55c35bb88341cbb59c51cc68794c1681
Vince Dorian <vince.jolene@gmail.com>
2026-07-10 23:03:25 +0800

    build(admin): 重新编译 admin 构建产物

---
+++ server/public/admin/js/404.Cy2-wrCH.js
@@ -0,0 +1,1 @@
+import r from"./error.C2tfDWz6.js";import{d as e,c as o,e as t,b as s}from"./index.DcHXnhA1.js";const a={class:"error404"},d=e({__name:"404",setup:e=>(e,d)=>(s(),o("div",a,[t(r,{code:"404",title:e.$t("errorPage.notFound")},null,8,["title"])]))});export{d as default};
